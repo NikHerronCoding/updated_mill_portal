6b93acf0a28c8e66642f4bfce5a1e583f541347f
Nik Herron <49733890+wolfpakk92@users.noreply.github.com>
2023-01-07 23:45:56 -0700

    Update settings.py

--- mill_portal/settings.py
+++ mill_portal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!m*-llhiz)%drebmo*71dca4v*in2$&9l$jd8x&!^9c9y+#$l%'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
